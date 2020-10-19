28950dff54a0f824d348e5690f1188d6fee70f7d
Cancio <filipe.cancio@gmail.com>
2020-10-19 15:29:39 -0300

    feat: create the pages #2

---
+++ src/pages/Sent/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+
+function Sent(){
+    return(
+        <>
+            <View>
+                <Text>Sent</Text>
+            </View>
+        </>
+    );
+}
+
+export default Sent;
