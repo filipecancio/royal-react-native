7d76c25d3193bb4711231d8236c3b8e9ea95c2c7
Cancio <filipe.cancio@gmail.com>
2020-10-20 09:57:39 -0300

    feat: finish navigation settings #2

--- src/pages/Sent/index.tsx
+++ src/pages/Sent/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Text, View } from 'react-native';
 import { Container } from './styles';
+import {useNavigation} from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 function Sent(){
+    const {navigate} = useNavigation();
+
+    function goHome(){
+        navigate('Home');
+    }
+
     return(
         <>
             <Container>
                 <Text>Sent</Text>
+                <TouchableOpacity onPress={goHome}>
+                    <Text>back</Text>
+                </TouchableOpacity>
             </Container>
         </>
     );
